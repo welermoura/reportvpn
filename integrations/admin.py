87db934b9e0de38dec27609fa174c3b12bbb0904
welermoura <155203871+welermoura@users.noreply.github.com>
2026-03-02 11:09:46 -0300

    Fix NameError in admin.py after cleanup

--- integrations/admin.py
+++ integrations/admin.py
@@ -1,4 +1,41 @@
+from django.contrib import admin
 from .models import FortiAnalyzerConfig, ActiveDirectoryConfig
+
+class SingletonModelAdmin(admin.ModelAdmin):
+    """
+    Prevents deletion and adding new instances if one already exists.
+    """
+    def has_add_permission(self, request):
+        if self.model.objects.exists():
+            return False
+        return super().has_add_permission(request)
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+@admin.register(FortiAnalyzerConfig)
+class FortiAnalyzerConfigAdmin(SingletonModelAdmin):
+    list_display = ('host', 'port', 'adom', 'verify_ssl')
+    fieldsets = (
+        ('Conexão', {
+            'fields': ('host', 'port', 'adom', 'verify_ssl')
+        }),
+        ('Autenticação', {
+            'fields': ('api_token', 'trusted_countries', 'is_enabled')
+        }),
+    )
+
+@admin.register(ActiveDirectoryConfig)
+class ActiveDirectoryConfigAdmin(SingletonModelAdmin):
+    list_display = ('server', 'port', 'use_ssl', 'base_dn')
+    fieldsets = (
+        ('Servidor', {
+            'fields': ('server', 'port', 'use_ssl')
+        }),
+        ('Autenticação e Busca', {
+            'fields': ('base_dn', 'bind_user', 'bind_password')
+        }),
+    )
 
 # Customized Group Admin to allow AD Search
 from django.contrib.auth.models import Group
